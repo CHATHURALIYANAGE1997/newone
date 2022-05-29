2bda727216caca4115a36ba01e6b6f39ab6299a9
Chathura Liyanage <84259289+CHATHURALIYANAGE1997@users.noreply.github.com>
2022-05-29 15:24:05 +0530

    Update HomeController.java

--- src/main/java/com/example/SpringbootLoginUserRegistration/controller/HomeController.java
+++ src/main/java/com/example/SpringbootLoginUserRegistration/controller/HomeController.java
@@ -17,7 +17,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import javax.servlet.http.HttpServletRequest;
 import java.util.Map;
 
-@CrossOrigin
+
 @Controller
 public class HomeController {
 
