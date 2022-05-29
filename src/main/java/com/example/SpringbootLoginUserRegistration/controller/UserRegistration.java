cd72def8979db29d76b37d6c869b6d26642183b9
Chathura Liyanage <84259289+CHATHURALIYANAGE1997@users.noreply.github.com>
2022-05-29 15:52:20 +0530

    Update UserRegistration.java

--- src/main/java/com/example/SpringbootLoginUserRegistration/controller/UserRegistration.java
+++ src/main/java/com/example/SpringbootLoginUserRegistration/controller/UserRegistration.java
@@ -7,11 +7,9 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
-@RequestMapping(path="group")
+@Controller
 public class UserRegistration {
 
     @Autowired
