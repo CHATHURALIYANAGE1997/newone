47ba732863e085c135d6dc7c2efbe18087c1a5c7
Chathura Liyanage <84259289+CHATHURALIYANAGE1997@users.noreply.github.com>
2022-05-29 15:45:08 +0530

    Update UserRegistration.java

--- src/main/java/com/example/SpringbootLoginUserRegistration/controller/UserRegistration.java
+++ src/main/java/com/example/SpringbootLoginUserRegistration/controller/UserRegistration.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping(path="group-project-3rd.herokuapp.com")
+@RequestMapping(path="group")
 public class UserRegistration {
 
     @Autowired
